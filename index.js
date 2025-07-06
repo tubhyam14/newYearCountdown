cb82330bbd211cd8daa5c3f28f30a02535f985a1
Tubhyam14 <tubhyamjain14@gmail.com>
2025-07-06 17:36:43 +0530

    fix crash issue

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const dayEl=document.getElementById("day");
-const hourEl=document.getElementById("hour");
-const minuteEl=document.getElementById("minute");
-const secondEl=document.getElementById("second");
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
 
-const newYear=new Date("Jan 01 2026 00:00:00");
-const second=1000;
-const minute=second*60;
-const hour=minute*60;
-const day=hour*24;
+const newYear = new Date("Jan 01 2026 00:00:00");
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
-update();
-function update(){
-    const now=new Date();
-    const gap=newYear-now;
-    dayEl.textContent=Math.floor(gap/day).toString().padStart(2,"0");
-    hourEl.textContent=Math.floor((gap%day)/hour).toString().padStart(2,"0");;
-    minuteEl.textContent=Math.floor((gap%hour)/minute).toString().padStart(2,"0");;
-    secondEl.textContent=Math.floor((gap%minute)/second).toString().padStart(2,"0");;
+function update() {
+    const now = new Date();
+    const gap = newYear - now;
 
-    setInterval(update,1000);
+    dayEl.textContent = Math.floor(gap / day).toString().padStart(2, "0");
+    hourEl.textContent = Math.floor((gap % day) / hour).toString().padStart(2, "0");
+    minuteEl.textContent = Math.floor((gap % hour) / minute).toString().padStart(2, "0");
+    secondEl.textContent = Math.floor((gap % minute) / second).toString().padStart(2, "0");
 }
+
+update(); 
+setInterval(update, 1000); 
