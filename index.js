2ab8a2c4092288b99f255e490aaffee271af3e7c
Tubhyam14 <tubhyamjain14@gmail.com>
2025-07-06 17:22:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const dayEl=document.getElementById("day");
+const hourEl=document.getElementById("hour");
+const minuteEl=document.getElementById("minute");
+const secondEl=document.getElementById("second");
+
+const newYear=new Date("Jan 01 2026 00:00:00");
+const second=1000;
+const minute=second*60;
+const hour=minute*60;
+const day=hour*24;
+
+update();
+function update(){
+    const now=new Date();
+    const gap=newYear-now;
+    dayEl.textContent=Math.floor(gap/day).toString().padStart(2,"0");
+    hourEl.textContent=Math.floor((gap%day)/hour);
+    minuteEl.textContent=Math.floor((gap%hour)/minute);
+    secondEl.textContent=Math.floor((gap%minute)/second);
+
+    setInterval(update,1000);
+}
